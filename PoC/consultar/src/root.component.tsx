24ccaccb43ad70e752b8d5102ab604edad865d03
wilson yesid rivera casas <wriver1@BOMC8092062.local>
2021-03-03 17:42:31 -0500

    aplicacion funcional localmente

--- PoC/consultar/src/root.component.tsx
+++ PoC/consultar/src/root.component.tsx
@@ -1,5 +1,53 @@
 import React from "react";
+import axios from "axios";
+import "./root.component.css";
 
-export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+var state = {
+  transactions: [],
+};
+class Root extends React.Component {
+  constructor(props) {
+    super(props);
+
+    axios
+      .get(
+        `https://ohtvdb3zu4.execute-api.us-east-1.amazonaws.com/poc/transaction`
+      )
+      .then((res) => {
+        const transactions = res.data;
+        state.transactions = transactions.Result.Items;
+        this.setState({ transactions });
+      });
+    console.log(state.transactions);
+  }
+
+  render() {
+    return (
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Mount</th>
+              <th>Channel</th>
+              <th>Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {state.transactions.map((transaction, index) => {
+              return (
+                <tr>
+                  <td>{transaction.TransactionId}</td>
+                  <td>{transaction.Mount}</td>
+                  <td>{transaction.Channel}</td>
+                  <td>{transaction.RequestTime}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
+export default Root;
--- PoC/registrar/src/root.component.tsx
+++ PoC/registrar/src/root.component.tsx
@@ -1,55 +1,82 @@
 import React from "react";
 import "./root.component.css";
-import axios from 'axios';
+import axios from "axios";
 
-const state = {
-  mount: 0,
-  channel: ''
+var state = {
+  Mount: 0,
+  Channel: "bancamovil",
 };
 
-export default function Root(props) {
+class Root extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-  return (
-    <div>
-      <form onSubmit={this.handleSubmit}>
-        <label htmlFor="mount">Monto</label>
-        <input type="text" name="mount" onChange={this.handleChange} placeholder="Ingrese el monto, sin puntos, sin comas" />
+  handleChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
 
-        <label>Canal</label>
-        <select name="channel" onChange={this.handleChange}>
-          <option value="bancamovil">Banca Movil</option>
-          <option value="servilinea">Servilinea</option>
-          <option value="cajero">Cajero</option>
-          <option value="datafono">Datafono</option>
-        </select>
+    if (target.type === "number") state.Mount = value;
+    else state.Channel = value;
 
-        <button type="submit">Registrar</button>
-      </form>
-    </div>
-  );
+    this.setState({
+      [name]: value,
+    });
+  }
 
+  handleSubmit(event) {
+    axios
+      .post(
+        `https://ohtvdb3zu4.execute-api.us-east-1.amazonaws.com/poc/transaction`,
+        state
+      )
+      .then((res) => {
+        console.log(res);
+        console.log(res.data);
+        alert("registro exitoso!!!");
+        state = {
+          Mount: 0,
+          Channel: "bancamovil",
+        };
+        this.setState(state);
+      });
+
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="Mount">Monto</label>
+          <input
+            type="number"
+            name="Mount"
+            value={state.Mount}
+            onChange={this.handleChange}
+            placeholder="Ingrese el monto, sin puntos, sin comas"
+          />
+
+          <label>Canal</label>
+          <select
+            name="Channel"
+            value={state.Channel}
+            onChange={this.handleChange}
+          >
+            <option value="bancamovil">Banca Movil</option>
+            <option value="servilinea">Servilinea</option>
+            <option value="cajero">Cajero</option>
+            <option value="datafono">Datafono</option>
+          </select>
+
+          <button type="submit">Registrar</button>
+        </form>
+      </div>
+    );
+  }
 }
 
-function handleChange(event) {
-  console.log("llego el evento");
-  this.setState({ name: event.target.value });
-};
-
-  
-
-function handleSubmit(event) {
-  event.preventDefault();
-
-  const transaction = {
-    mount: this.state.mount,
-    channel: this.state.channel
-  };
-
-  console.log("llegó la peticion")
-
-  axios.post(`https://jsonplaceholder.typicode.com/users`, { transaction })
-    .then(res => {
-      console.log(res);
-      console.log(res.data);
-    })
-};
+export default Root;
